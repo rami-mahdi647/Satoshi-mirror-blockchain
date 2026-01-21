1142c8f3db3fdeee04d97794cd799bd333c049bd
rami-mahdi647 <rami.mahdi.nabhan@proton.me>
2026-01-21 14:18:57 +0000

    Add __init__.py for project source package

--- Src/_init_.py
+++ Src/_init_.py
@@ -1,7 +1,1 @@
-diff --git a/src/__init__.py b/src/__init__.py
-new file mode 100644
-index 0000000000000000000000000000000000000000..4cd23c199bca480a31ccb658bd51cb5094121475
---- /dev/null
-+++ b/src/__init__.py
-@@ -0,0 +1 @@
-+"""Project source package."""
+"""Project source package."""
